95975ad3597cc4d72b98470199d3adc8e90ee831
eraltafs <43698833+eraltafs@users.noreply.github.com>
2023-06-01 15:45:57 +0530

    frontend completed

---
+++ client/src/index.js
@@ -0,0 +1,101 @@
+let locationHeading = document.getElementById("loacationspan")
+let top_temp = document.getElementById("top_temp");
+let top_temptext = document.getElementById("top_temptext");
+let top_mintemp = document.getElementById("top_mintemp");
+let top_maxtemp = document.getElementById("top_maxtemp");
+let sunriseContent = document.getElementById("top_sunrise");
+let sunsetContent = document.getElementById("top_sunset");
+let humidity = document.getElementById("humidity");
+let windspeed = document.getElementById("top_windspeed");
+let inputbox = document.getElementById("city")
+
+locationHeading.textContent = "Delhi"
+let form = document.querySelector("form");
+
+form.addEventListener("submit", (even) => {
+  even.preventDefault();
+  let city = form.city.value
+  if(city){
+
+      let result = getData(city)
+      if(result){
+        localStorage.setItem("city",city)
+      }else{
+        alert("please input valid city")
+      }
+  }else{
+    alert("please input city name")
+  }
+
+});
+window.onload = async () => {
+  let city = localStorage.getItem("city")||"delhi"
+  inputbox.value = localStorage.getItem("city")
+  locationHeading.textContent = localStorage.getItem("city")
+  getData(city);
+};
+const getData = async (city) => {
+  const res = await fetch(`http://localhost:8000/weather?city=${city}`);
+  data = await res.json();
+  if (data.error === "city not found") {
+    localStorage.setItem("city", "delhi");
+    alert("invalid city name, try again please");
+    return false
+
+  }
+
+  weather = data.weather;
+  append(data.weather.forecast);
+  top_temp.textContent = weather.current.temperature;
+  sunriseContent.textContent = weather.current.sunrise;
+  sunsetContent.textContent = weather.current.sunset;
+  humidity.textContent = weather.current.humidity + "%";
+  windspeed.textContent = weather.current.wind + "km/hr";
+  top_temptext.textContent = weather.current.description;
+  top_mintemp.textContent = weather.current.temperature_min;
+  top_maxtemp.textContent = weather.current.temperature_max;
+  locationHeading.textContent = city
+  localStorage.setItem("city", city);
+  return true
+};
+const append = (data) => {
+  let forecastContainer = document.getElementById("forecast");
+  forecastContainer.innerHTML = null;
+  data.forEach((item, i) => {
+    if (i !== 0) {
+      let dateArray = item.date.split("-");
+
+      let div = document.createElement("div");
+      const dateStr = item.date;
+      const dateObj = new Date(dateStr);
+      const dayName = dateObj.toLocaleString("en-US", { weekday: "short" }); // Short day name (e.g., "Thu")
+      const monthName = dateObj.toLocaleString("en-US", { month: "short" }); // Short month name (e.g., "Jun")
+      let dayH2 = document.createElement("h2");
+      dayH2.textContent = dayName;
+      let monthH2 = document.createElement("h2");
+      monthH2.textContent = dateArray[2] + " " + monthName;
+
+      let img = document.createElement("img");
+
+      img.src = `https://www.hindustantimes.com/static-content/1m/2022/ht-images/${item.icon}.svg`;
+      if (item.icon[2] == "n") {
+        img.src = `https://www.hindustantimes.com/static-content/1m/2022/ht-images/${item.icon[0]}${item.icon[1]}d.svg`;
+      }
+
+      let temph2 = document.createElement("h2");
+      temph2.textContent = item.temperature;
+
+      let min_maxDiv = document.createElement("div");
+      min_maxDiv.className = "flexdivs spacing";
+      let mindiv = document.createElement("div");
+      mindiv.innerHTML = `<i class="fa-solid fa-angle-down"></i><h2>${item.temperature_maximum}</h2>`;
+      mindiv.className = "minmaxblur";
+      let maxdiv = document.createElement("div");
+      maxdiv.innerHTML = `<i class="fa-solid fa-angle-up"></i><h2>${item.temperature_minimum}</h2>`;
+      maxdiv.className = "minmaxblur";
+      min_maxDiv.append(maxdiv, mindiv);
+      div.append(dayH2, monthH2, img, temph2, min_maxDiv);
+      forecastContainer.append(div);
+    }
+  });
+};
--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const Redis = require("ioredis");
+const cors = require("cors");
 require("dotenv").config();
 
 const { authentication } = require("./middleware/authentication");
@@ -8,14 +9,14 @@ const { connection } = require("./config/db");
 const { userRouter } = require("./routes/user.routes");
 
 const app = express();
+app.use(cors());
 const redis = new Redis();
 
 app.use(express.json());
 app.use(cookieParser());
 app.use("/user", userRouter);
 
-
-app.get("/weather", authentication, async (req, res) => {
+app.get("/weather", async (req, res) => {
   const { city } = req.query;
 
   try {
@@ -25,36 +26,77 @@ app.get("/weather", authentication, async (req, res) => {
       res.json({ weather: weatherData });
     } else {
       const response = await fetch(
-        `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${process.env.api}`
+        `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${process.env.api}&cnt=50`
       );
       const data = await response.json();
-
-
       if (data.cod !== "200") {
-        
         const errorMessage = data.message || "Failed to fetch weather data";
         res.status(400).json({ error: errorMessage });
         return;
       }
 
+      const forecastData = {};
+      data.list.forEach((item) => {
+        const date = item.dt_txt.split(" ")[0]; // Extracting date from date-time string
+        if (!forecastData[date]) {
+          forecastData[date] = {
+            temperature: Math.floor(item.main.temp - 273.15),
+            temperature_minimum: Math.floor(item.main.temp_min - 273.15),
+            temperature_maximum: Math.round(item.main.temp_max - 273.15),
+            description: item.weather[0].description,
+            icon: item.weather[0].icon,
+            date: date,
+          };
+        } else {
+          if (item.main.temp_min < forecastData[date].temperature_min) {
+            forecastData[date].temperature_min = item.main.temp_min - 273.15;
+          }
+          if (item.main.temp_max > forecastData[date].temperature_max) {
+            forecastData[date].temperature_max = item.main.temp_max - 273.15;
+          }
+        }
+      });
+      wind =data.list[0].wind.speed;
+      const sunriseTimestamp = data.city.sunrise;
+      const sunsetTimestamp = data.city.sunset;
+
+      const sunriseTime = new Date(sunriseTimestamp * 1000).toLocaleTimeString(
+        "en-US",
+        {
+          hour: "2-digit",
+          minute: "2-digit",
+          hour12: true,
+        }
+      );
+
+      const sunsetTime = new Date(sunsetTimestamp * 1000).toLocaleTimeString(
+        "en-US",
+        {
+          hour: "2-digit",
+          minute: "2-digit",
+          hour12: true,
+        }
+      );
+
       const weatherData = {
         city: data.city.name,
         country: data.city.country,
         current: {
-          temperature: data.list[0].main.temp,
+          wind,
+          sunrise: sunriseTime,
+          sunset: sunsetTime,
+          humidity: data.list[0].main.humidity,
+          temperature: Math.floor(data.list[0].main.temp - 273.15),
+          temperature_min: Math.floor(data.list[0].main.temp_min - 273.15),
+          temperature_max: Math.floor(data.list[0].main.temp_max - 273.15),
           description: data.list[0].weather[0].description,
         },
-        forecast: data.list.slice(0).map((item) => ({
-          temperature: item.main.temp,
-          temperature_min: item.main.temp_min,
-          temperature_max: item.main.temp_max,
-          description: item.weather[0].description,
-          dateTime: item.dt_txt,
-        })),
+        forecast: Object.values(forecastData),
       };
 
-      await redis.set(city, JSON.stringify(weatherData),"EX", 30);
+      await redis.set(city, JSON.stringify(weatherData), "EX", 30);
 
+      console.log(data.list[0].weather);
       res.json({ weather: weatherData });
     }
   } catch (error) {
@@ -62,8 +104,6 @@ app.get("/weather", authentication, async (req, res) => {
     res.status(500).json({ message: "An error occurred" });
   }
 });
-
-
 
 app.listen(8000, async () => {
   try {
