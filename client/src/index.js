08d823ce67e60a57c1b03dc39352d4b3bedacb19
eraltafs <43698833+eraltafs@users.noreply.github.com>
2023-08-08 09:30:48 +0530

    channge temprature unit feature added

--- client/src/index.js
+++ client/src/index.js
@@ -1,4 +1,4 @@
-let api = "https://server-alphabin.onrender.com"
+let api = "https://server-alphabin.onrender.com";
 let locationHeading = document.getElementById("loacationspan");
 let top_temp = document.getElementById("top_temp");
 let top_temptext = document.getElementById("top_temptext");
@@ -9,14 +9,30 @@ let sunsetContent = document.getElementById("top_sunset");
 let humidity = document.getElementById("humidity");
 let windspeed = document.getElementById("top_windspeed");
 let inputbox = document.getElementById("city");
+let token = localStorage.getItem("token");
+let prefredlocations = document.getElementById("prefredlocations");
+
 locationHeading.textContent = "Delhi";
 let form = document.querySelector("form");
+window.onload = async () => {
+  let city = localStorage.getItem("city") || "delhi";
+  inputbox.value = localStorage.getItem("city");
+  locationHeading.textContent = localStorage.getItem("city");
+  getData(city);
+  if (token) getPreffred();
+  else prefredlocations.style.display = "none";
+};
 
 form.addEventListener("submit", (even) => {
   even.preventDefault();
   let city = form.city.value;
+  let temperatureUnit = document.getElementById("temperatureUnit").value;
   if (city) {
-    let result = getData(city);
+    if (temperatureUnit) {
+      result = getData(city, temperatureUnit);
+    } else {
+      result = getData(city);
+    }
     if (result) {
       localStorage.setItem("city", city);
     } else {
@@ -30,16 +46,15 @@ let savebtn = document.getElementById("savebtn");
 savebtn.onclick = async () => {
   let city = inputbox.value;
   let temperatureUnit = document.getElementById("temperatureUnit").value;
-  if (!localStorage.getItem("token")) {
+  if (!token) {
     alert("please login to save");
     location.href = "./loginsignup.html";
-  }
-  else if (city && temperatureUnit) {
+  } else if (city && temperatureUnit) {
     let res = await fetch(`${api}/user/preferences`, {
       method: "POST",
       body: JSON.stringify({ city, temperatureUnit }),
       headers: {
-        authorization: `Bearer ${localStorage.getItem("token")}`,
+        authorization: `Bearer ${token}`,
         "Content-Type": "application/json",
       },
     });
@@ -48,7 +63,6 @@ savebtn.onclick = async () => {
     alert("type city name to save");
   }
 };
-let prefredlocations = document.getElementById("prefredlocations");
 prefredlocations.onchange = () => {
   if (prefredlocations.value) {
     city = prefredlocations.value;
@@ -58,14 +72,15 @@ prefredlocations.onchange = () => {
 const getPreffred = async () => {
   let res = await fetch(`${api}/user/preferences`, {
     headers: {
-      authorization: `Bearer ${localStorage.getItem("token")}`,
+      authorization: `Bearer ${token}`,
       "Content-Type": "application/json",
     },
   });
   let data = await res.json();
   let { locations } = data;
-  appendlocations(locations);
- 
+  if (locations) {
+    appendlocations(locations);
+  }
 };
 const appendlocations = async (data) => {
   let option = document.createElement("option");
@@ -79,14 +94,7 @@ const appendlocations = async (data) => {
     prefredlocations.append(option);
   });
 };
-window.onload = async () => {
-  let city = localStorage.getItem("city") || "delhi";
-  inputbox.value = localStorage.getItem("city");
-  locationHeading.textContent = localStorage.getItem("city");
-  getData(city);
-  getPreffred();
-};
-const getData = async (city) => {
+const getData = async (city, temperatureUnit) => {
   const res = await fetch(`${api}/weather?city=${city}`);
   data = await res.json();
   if (data.error === "city not found") {
@@ -96,22 +104,31 @@ const getData = async (city) => {
   }
 
   weather = data.weather;
-  append(data.weather.forecast);
-  top_temp.textContent = weather.current.temperature;
-  sunriseContent.textContent = weather.current.sunrise;
-  sunsetContent.textContent = weather.current.sunset;
-  humidity.textContent = weather.current.humidity + "%";
-  windspeed.textContent = weather.current.wind + "km/hr";
-  top_temptext.textContent = weather.current.description;
-  top_mintemp.textContent = weather.current.temperature_min;
-  top_maxtemp.textContent = weather.current.temperature_max;
-  locationHeading.textContent = city;
-  localStorage.setItem("city", city);
+  if (temperatureUnit === "Farenhite") {
+    append(data.weather.forecast, temperatureUnit);
+    top_temp.textContent = (weather.current.temperature * 9) / 5 + 32;
+    top_mintemp.textContent = (weather.current.temperature_min * 9) / 5 + 32;
+    top_maxtemp.textContent = (weather.current.temperature_max * 9) / 5 + 32;
+  } else {
+    append(data.weather.forecast);
+
+    top_temp.textContent = weather.current.temperature;
+    sunriseContent.textContent = weather.current.sunrise;
+    sunsetContent.textContent = weather.current.sunset;
+    humidity.textContent = weather.current.humidity + "%";
+    windspeed.textContent = weather.current.wind + "km/hr";
+    top_temptext.textContent = weather.current.description;
+    top_mintemp.textContent = weather.current.temperature_min;
+    top_maxtemp.textContent = weather.current.temperature_max;
+    locationHeading.textContent = city;
+    localStorage.setItem("city", city);
+  }
   return true;
 };
-const append = (data) => {
+const append = (data, temperatureUnit) => {
   let forecastContainer = document.getElementById("forecast");
   forecastContainer.innerHTML = null;
+
   data.forEach((item, i) => {
     if (i !== 0) {
       let dateArray = item.date.split("-");
@@ -132,19 +149,27 @@ const append = (data) => {
       if (item.icon[2] == "n") {
         img.src = `https://www.hindustantimes.com/static-content/1m/2022/ht-images/${item.icon[0]}${item.icon[1]}d.svg`;
       }
-
       let temph2 = document.createElement("h2");
-      temph2.textContent = item.temperature;
-
       let min_maxDiv = document.createElement("div");
       min_maxDiv.className = "flexdivs spacing";
       let mindiv = document.createElement("div");
-      mindiv.innerHTML = `<i class="fa-solid fa-angle-down"></i><h2>${item.temperature_maximum}</h2>`;
       mindiv.className = "minmaxblur";
       let maxdiv = document.createElement("div");
-      maxdiv.innerHTML = `<i class="fa-solid fa-angle-up"></i><h2>${item.temperature_minimum}</h2>`;
       maxdiv.className = "minmaxblur";
-      min_maxDiv.append(maxdiv, mindiv);
+      if (temperatureUnit === "Farenhite") {
+        temph2.textContent = (item.temperature * 9) / 5 + 32;
+        mindiv.innerHTML = `<i class="fa-solid fa-angle-down"></i><h2>${
+          (item.temperature_minimum * 9) / 5 + 32
+        }</h2>`;
+        maxdiv.innerHTML = `<i class="fa-solid fa-angle-up"></i><h2>${
+          (item.temperature_maximum * 9) / 5 + 32
+        }</h2>`;
+      } else {
+        temph2.textContent = item.temperature;
+        mindiv.innerHTML = `<i class="fa-solid fa-angle-down"></i><h2>${item.temperature_minimum}</h2>`;
+        maxdiv.innerHTML = `<i class="fa-solid fa-angle-up"></i><h2>${item.temperature_maximum}</h2>`;
+      }
+      min_maxDiv.append(mindiv, maxdiv);
       div.append(dayH2, monthH2, img, temph2, min_maxDiv);
       forecastContainer.append(div);
     }
